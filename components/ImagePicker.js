400428f062d357549a01d2a7dacd3d81c6c618bb
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-25 22:41:55 +0930

    setup the camera from device

---
+++ components/ImagePicker.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { View, Button, Text, StyleSheet, Image, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Colors from "../constants/Colors";
+
+const ImgPicker = (props) => {
+  const verifyPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA);
+    if (result.status !== "granted") {
+      Alert.alert(
+        "Insufficient permission!",
+        "You need to grant camera permission to use the app",
+        [{ text: "Okay" }]
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission){
+      return;
+    }
+    ImagePicker.launchCameraAsync();
+  };
+
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        <Text>Np image yet.</Text>
+        <Image style={styles.image} />
+      </View>
+      <Button
+        title="Take Image"
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: "center",
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImgPicker;
