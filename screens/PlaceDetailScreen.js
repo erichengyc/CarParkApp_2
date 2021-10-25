3edd09f75ee87c1c655f5cedb09b64bb80cec07f
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-25 21:48:20 +0930

    output the list of places

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const PlaceDetailScreen = props => {
-    return <View>
-        <Text>PlaceDetailScreen</Text>
+const PlaceDetailScreen = (props) => {
+  return (
+    <View>
+      <Text>PlaceDetailScreen</Text>
     </View>
+  );
+};
+
+PlaceDetailScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: navData.navigation.getParam("placeTitle"),
+  };
 };
 
 const styles = StyleSheet.create({});
